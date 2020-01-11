0b3ec6d4abc52627a290afba1b7fa056a0e906e6
omega-Lee <2033211717@qq.com>
2020-01-11 21:33:02 +0800

    减色算法

--- A6.cpp
+++ A6.cpp
@@ -8,6 +8,7 @@ void A6(void)
 	/*原理：
 	假设N是减色因子，将图像中每个像素的值除以N(使用整数除法，不保留余数。实际上，该算法就是利用了整数除法的特性来实现减色的)。然后将结果乘以N，得到N的倍数，再加上N/ 2，就得到相邻的N的倍数之间的中间值。对所有8位通道值重复这个过程，就会得到(256 / N)∗(256 / N)∗(256 / N)种可能的颜色值。
 	*/
+
 	printf_s("减色处理\n");
 	Mat imgSrc = imread("C:\\Users\\Administrator\\Desktop\\img2.jpg");
 	int imgHeight = imgSrc.rows;
