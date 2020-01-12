b10531ae3f82b6692ebf097bcc56c2e983628d0b
omega-Lee <2033211717@qq.com>
2020-01-12 17:50:20 +0800

    中值滤波

--- A10.cpp
+++ A10.cpp
@@ -8,7 +8,7 @@ using namespace cv;
 void A10()
 {
 	printf_s("中值滤波\n");
-	Mat imgSrc = imread("C:\\Users\\Administrator\\Desktop\\img3.jpg");
+	Mat imgSrc = imread("C:\\Users\\Administrator\\Desktop\\OpencvTestImg\\img512.png");
 	int imgHeight = imgSrc.rows;
 	int imgWidth = imgSrc.cols;
 	int channel = imgSrc.channels();
@@ -17,35 +17,35 @@ void A10()
 	const int kernelSize = 3;//卷积核大小
 	int kernelRadius = floor(kernelSize / 2);//卷积核半径
 	float kernelArray[kernelSize * kernelSize];//卷积核矩阵,排序矩阵
-	int count = 0;//计数器
+
+	Mat imgtemp = Mat::zeros(imgHeight + 2 * kernelRadius, imgWidth + 2 * kernelRadius, CV_8UC3);
+	//复制一张原图，并且添加边框
+	for (int y = 0; y < imgHeight; y++)
+		for (int x = 0; x < imgWidth; x++)
+			for (int c = 0; c < channel; c++)
+				imgtemp.at<Vec3b>(y + kernelRadius, x + kernelRadius)[c] = imgSrc.at<Vec3b>(y, x)[c];
 
 	for (int y = 0; y < imgHeight; ++y)
 	{
-		for(int x=0;x<imgWidth;++x)
+		for (int x = 0; x < imgWidth; ++x)
 		{
 			for (int c = 0; c < channel; ++c)
 			{
+				double val = 0;
+				int count = 0;
 				//防止倒序
-				for (int i = 0; i < kernelSize * kernelSize; i++) {
+				for (int i = 0; i < kernelSize * kernelSize; i++)
 					kernelArray[i] = 999;
-				}
-
 				//在卷积核范围内取值
 				for (int dy = -kernelRadius; dy < kernelRadius + 1; dy++)
-				{
-					for (int dx = -kernelRadius; dx < kernelRadius+1; dx++)
-					{ 
+					for (int dx = -kernelRadius; dx < kernelRadius + 1; dx++)
 						if (((y + dy) >= 0) && ((x + dx) >= 0))
-						{
-							kernelArray[count++] = (int)imgSrc.at<Vec3b>(y + dy, x + dx)[c];
-						}
-					}
-				}
+							kernelArray[count++] = (int)imgtemp.at<Vec3b>(y + dy, x + dx)[c];
 
 				//对元素进行排序
-				std::sort(kernelArray,kernelArray+(kernelSize*kernelSize));
+				std::sort(kernelArray, kernelArray + (kernelSize * kernelSize));
 				//取中值
-				int val = kernelArray[int(floor(count/2))+1];
+				val = kernelArray[int(floor(count / 2)) + 1];
 				imgOut.at<Vec3b>(y, x)[c] = (uchar)val;
 			}
 		}
