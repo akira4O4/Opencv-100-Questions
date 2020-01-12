1ea128c9b8f8bd91f3a1ecdfc6bddfe56a948348
omega-Lee <2033211717@qq.com>
2020-01-12 22:34:36 +0800

    main文件

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include "A_1_10.h"
+#include "A_11_20.h"
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat imgSrc1 = imread("C:\\Users\\Administrator\\Desktop\\OpencvTestImg\\img3.jpg");
+	Mat imgSrc512 = imread("C:\\Users\\Administrator\\Desktop\\OpencvTestImg\\img512.png");
+	Mat imgSrc32 = imread("C:\\Users\\Administrator\\Desktop\\OpencvTestImg\\img32.png");
+	//A1();
+	//A2();
+	//A3();
+	//A4();
+	//A5();
+	//A6();
+	//A7();
+	//A8();
+	//A9();
+	//A10();
+	//A11(imgSrc2);
+	//A12(imgSrc1);
+	//A13(imgSrc512);
+	//A14(imgSrc512);
+	A15(imgSrc512);
+
+	return 0;
+}
