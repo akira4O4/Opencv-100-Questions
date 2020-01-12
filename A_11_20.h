3522357d0c876845ae7f0fb6582e94bef6f0d116
omega-Lee <2033211717@qq.com>
2020-01-12 14:58:49 +0800

    11-20题头文件

---
+++ A_11_20.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef _Q_11_20_
+#define _Q_11_20_
+
+
+int imgHeight;
+int imgWidth;
+
+void A11(void);
+void A12(void);
+void A13(void);
+void A14(void);
+void A15(void);
+void A16(void);
+void A17(void);
+void A18(void);
+void A19(void);
+void A20(void);
+
+#endif
