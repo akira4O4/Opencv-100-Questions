023f9e2989027ed188ba711209ac259f26d25865
omega-Lee <2033211717@qq.com>
2020-01-15 21:19:40 +0800

    21-30题头文件

---
+++ A_21_30.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#ifndef _A_21_30_
+#define _A_21_30_
+using namespace cv;
+
+void A21(Mat img);
+void A22(Mat img);
+void A23(Mat img);
+void A24(Mat img);
+void A25(Mat img);
+void A26(Mat img);
+void A27(Mat img);
+void A28(Mat img);
+void A29(Mat img);
+void A30(Mat img);
+
+
+#endif // !_A_21_30_
