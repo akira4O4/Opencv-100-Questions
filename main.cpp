0d817964774fdda6fdc5bba48abedcd9a7edc760
omega-Lee <2033211717@qq.com>
2020-01-15 22:18:35 +0800

    test A21

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/opencv.hpp>
 #include "A_1_10.h"
 #include "A_11_20.h"
+#include "A_21_30.h"
 
 using namespace std;
 using namespace cv;
@@ -12,6 +13,7 @@ int main()
 	Mat imgSrc1 = imread("C:\\Users\\Administrator\\Desktop\\OpencvTestImg\\img3.jpg");
 	Mat imgSrc512 = imread("C:\\Users\\Administrator\\Desktop\\OpencvTestImg\\img512.png");
 	Mat imgSrc32 = imread("C:\\Users\\Administrator\\Desktop\\OpencvTestImg\\img32.png");
+	Mat img_dark = imread("C:\\Users\\Administrator\\Desktop\\OpencvTestImg\\img_dark.jpg");
 	//A1();
 	//A2();
 	//A3();
@@ -32,6 +34,7 @@ int main()
 	//A18(imgSrc512);
 	//A19(imgSrc512);
 	//A20(imgSrc512);
+	A21(img_dark);
 
 	return 0;
 }
