629cfe7da77b588aa231ef3535d6a72a5c389ccc
omega-Lee <2033211717@qq.com>
2020-01-18 12:48:14 +0800

    31-40头文件

---
+++ A_31_40.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#ifndef _A_31_40_
+#define _A_31_40_
+using namespace cv;
+
+void A31(Mat img);
+void A32(Mat img);
+void A33(Mat img);
+void A34(Mat img);
+void A35(Mat img);
+void A36(Mat img);
+void A37(Mat img);
+void A38(Mat img);
+void A39(Mat img);
+void A40(Mat img);
+
+
+#endif // !_A_31_40_
