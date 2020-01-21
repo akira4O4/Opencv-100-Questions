f30748abb2450be1a8fb9f65663217320f261fff
omega-Lee <2033211717@qq.com>
2020-01-21 21:03:07 +0800

    41-50题头文件

---
+++ A_41_50.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#ifndef _A_41_50_
+#define _A_41_50_
+using namespace cv;
+
+void A41(Mat img);
+void A42(Mat img);
+void A43(Mat img);
+void A44(Mat img);
+void A45(Mat img);
+void A46(Mat img);
+void A47(Mat img);
+void A48(Mat img);
+void A49(Mat img);
+void A50(Mat img);
+
+
+#endif // !_A_41_50_
