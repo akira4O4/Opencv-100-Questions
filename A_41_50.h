86d87f3baa197253315c0432d6744e71517f93c0
omega-Lee <2033211717@qq.com>
2020-01-22 14:56:47 +0800

    为了方便调用，把canny函数写进头文件

--- A_41_50.h
+++ A_41_50.h
@@ -7,6 +7,23 @@
 
 using namespace cv;
 
+//Canny Function
+//灰度化
+Mat ImgGray(Mat img);
+float Clip(float value, float min, float max);
+//高斯滤波
+Mat GaussianFilter(Mat img, double Sigma);
+//Sobel垂直
+Mat SobelFilterV(Mat img);
+//Sobel水平
+Mat SobelFilterH(Mat img);
+Mat GetEdge(Mat imgX, Mat imgY);
+Mat GetAngel(Mat imgX, Mat imgY);
+//非极大值抑制：NMS
+Mat NMS(Mat angle, Mat edge);
+//连接边
+Mat Histerisis(Mat edge, int HT, int LT);
+
 void A41(Mat img);
 void A42(Mat img);
 void A43(Mat img);
