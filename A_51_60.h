321c883f7551ffb836c29f59e7c0de27a48c8872
omega-Lee <2033211717@qq.com>
2020-01-23 13:07:08 +0800

    51-60的头文件

---
+++ A_51_60.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#ifndef _A_51_60_
+#define _A_51_60_
+
+#define PI  atan(1) * 4
+
+using namespace cv;
+void A51(Mat img);
+void A52(Mat img);
+void A53(Mat img);
+void A54(Mat img);
+void A55(Mat img);
+void A56(Mat img);
+void A57(Mat img);
+void A58(Mat img);
+void A59(Mat img);
+void A60(Mat img);
+#endif // !_A_51_60_
