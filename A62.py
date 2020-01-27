eaaabb9b3e4c0be3f0c2d1932e293d817d5c06de
omega-Lee <2033211717@qq.com>
2020-01-27 22:56:09 +0800

    8连接数-py

---
+++ A62.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = "C:/Users/Administrator/Desktop/OpencvTestImg/img10.png"
+
+
+# connect 8
+def connect_8(img):
+    img = cv2.imread(img).astype(np.float32)
+    # get shape
+    H, W, C = img.shape
+
+    # prepare temporary
+    _tmp = np.zeros((H, W), dtype=np.int)
+
+    # get binarize
+    _tmp[img[..., 0] > 0] = 1
+
+    # inverse for connect 8
+    tmp = 1 - _tmp
+
+    # prepare image
+    out = np.zeros((H, W, 3), dtype=np.uint8)
+
+    # each pixel
+    for y in range(H):
+        for x in range(W):
+            if _tmp[y, x] < 1:
+                continue
+
+            S = 0
+            S += (tmp[y, min(x + 1, W - 1)] - tmp[y, min(x + 1, W - 1)] * tmp[max(y - 1, 0), min(x + 1, W - 1)] * tmp[
+                max(y - 1, 0), x])
+            S += (tmp[max(y - 1, 0), x] - tmp[max(y - 1, 0), x] * tmp[max(y - 1, 0), max(x - 1, 0)] * tmp[
+                y, max(x - 1, 0)])
+            S += (tmp[y, max(x - 1, 0)] - tmp[y, max(x - 1, 0)] * tmp[min(y + 1, H - 1), max(x - 1, 0)] * tmp[
+                min(y + 1, H - 1), x])
+            S += (tmp[min(y + 1, H - 1), x] - tmp[min(y + 1, H - 1), x] * tmp[min(y + 1, H - 1), min(x + 1, W - 1)] *
+                  tmp[y, min(x + 1, W - 1)])
+
+            if S == 0:
+                out[y, x] = [0, 0, 255]
+            elif S == 1:
+                out[y, x] = [0, 255, 0]
+            elif S == 2:
+                out[y, x] = [255, 0, 0]
+            elif S == 3:
+                out[y, x] = [255, 255, 0]
+            elif S == 4:
+                out[y, x] = [255, 0, 255]
+
+    out = out.astype(np.uint8)
+    # Save result
+    cv2.imwrite("out.png", out)
+    cv2.imshow("result", out)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    connect_8(img)
