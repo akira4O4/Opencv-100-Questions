61778a17d6dd3b2173257ac94b47491ce8641f81
omega-Lee <2033211717@qq.com>
2020-01-27 18:31:00 +0800

    61-70题头文件

---
+++ A_61_70.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#ifndef _A_61_70_
+#define _A_61_70_
+
+#define PI  atan(1) * 4
+
+using namespace cv;
+void A61(Mat img);
+void A62(Mat img);
+void A63(Mat img);
+void A64(Mat img1);
+void A65(Mat img1);
+void A66(Mat img);
+void A67(Mat img1);
+void A68(Mat img);
+void A69(Mat img);
+void A70(Mat img);
+#endif 
