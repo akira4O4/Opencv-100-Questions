c049cc049d253b3064934753f0d61cb2f92ca5c3
omega-Lee <2033211717@qq.com>
2020-01-29 11:51:52 +0800

    8连接数-py

--- A62.py
+++ A62.py
@@ -10,36 +10,36 @@ def connect_8(img):
     img = cv2.imread(img).astype(np.uint8)
     H, W, C = img.shape
 
-    _tmp = np.zeros((H, W), dtype=np.uint8)
+    bin = np.zeros((H, W), dtype=np.uint8)
 
-    _tmp[img[..., 0] > 0] = 1
+    bin[img[..., 0] > 0] = 1
 
-    tmp = 1 - _tmp
+    img8 = 1 - bin
 
     out = np.zeros((H, W, 3), dtype=np.uint8)
 
     # each pixel
     for y in range(H):
         for x in range(W):
-            if _tmp[y, x] < 1:
+            if bin[y, x] < 1:
                 continue
 
             S = 0
-            S += (tmp[y, min(x + 1, W - 1)] - tmp[y, min(x + 1, W - 1)] * tmp[max(y - 1, 0), min(x + 1, W - 1)] * tmp[
+            S += (img8[y, min(x + 1, W - 1)] - img8[y, min(x + 1, W - 1)] * img8[max(y - 1, 0), min(x + 1, W - 1)] * img8[
                 max(y - 1, 0), x])
-            S += (tmp[max(y - 1, 0), x] - tmp[max(y - 1, 0), x] * tmp[max(y - 1, 0), max(x - 1, 0)] * tmp[
+            S += (img8[max(y - 1, 0), x] - img8[max(y - 1, 0), x] * img8[max(y - 1, 0), max(x - 1, 0)] * img8[
                 y, max(x - 1, 0)])
-            S += (tmp[y, max(x - 1, 0)] - tmp[y, max(x - 1, 0)] * tmp[min(y + 1, H - 1), max(x - 1, 0)] * tmp[
+            S += (img8[y, max(x - 1, 0)] - img8[y, max(x - 1, 0)] * img8[min(y + 1, H - 1), max(x - 1, 0)] * img8[
                 min(y + 1, H - 1), x])
-            S += (tmp[min(y + 1, H - 1), x] - tmp[min(y + 1, H - 1), x] * tmp[min(y + 1, H - 1), min(x + 1, W - 1)] *
-                  tmp[y, min(x + 1, W - 1)])
+            S += (img8[min(y + 1, H - 1), x] - img8[min(y + 1, H - 1), x] * img8[min(y + 1, H - 1), min(x + 1, W - 1)] *
+                  img8[y, min(x + 1, W - 1)])
 
             if S == 0:
-                out[y, x] = [0, 0, 255]
+                out[y, x] = [0, 0, 255]#b
             elif S == 1:
-                out[y, x] = [0, 255, 0]
+                out[y, x] = [0, 255, 0]#g
             elif S == 2:
-                out[y, x] = [255, 0, 0]
+                out[y, x] = [255, 0, 0]#r
             elif S == 3:
                 out[y, x] = [255, 255, 0]
             elif S == 4:
@@ -47,7 +47,7 @@ def connect_8(img):
 
     cv2.imshow("result", out)
     cv2.imshow("src", img)
-    cv2.imshow("tmp", tmp)
+    cv2.imshow("img8", img8)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
