33d6f84375b8b4e7e2f17ce011cda4bf077d4978
omega-Lee <2033211717@qq.com>
2020-01-31 23:32:18 +0800

    71-80题头文件

---
+++ A_71_80.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#ifndef _A_71_70_
+#define _A_71_70_
+
+#define PI  atan(1) * 4
+
+using namespace cv;
+
+void A71(Mat img);
+void A72(Mat img);
+void A73(Mat img);
+void A74(Mat img1);
+void A75(Mat img1);
+void A77(Mat img);
+void A77(Mat img1);
+void A78(Mat img);
+void A79(Mat img);
+void A80(Mat img);
+#endif 
