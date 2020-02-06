6b8b8da8c31030c7b6aea517fcf972240e1d1e54
omega-Lee <2033211717@qq.com>
2020-02-06 21:59:36 +0800

    test a38

--- main.cpp
+++ main.cpp
@@ -75,7 +75,8 @@ int main()
 	//A34(imgSrc128);
 	//A35(imgSrc128);
 	//A36(imgSrc128);
-	A37(imgSrc128);
+	//A37(imgSrc128);
+	A38(imgSrc128);
 	//A41(imgSrc512);
 	//A42(imgSrc512);
 	//A43(img_canny);
